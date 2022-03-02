0bd35c60cb0559eb904ad7912d53fe4c43189dab
Gun Gun Priatna <gungunpriatna02@gmail.com>
2022-03-03 06:28:26 +0700

    test: user can create a post

--- tests/Feature/ManagePostsTest.php
+++ tests/Feature/ManagePostsTest.php
@@ -12,7 +12,30 @@ class ManagePostsTest extends TestCase
     /** @test */
     public function user_can_create_a_post()
     {
-        $this->assertTrue(true);
+        // user buka halaman create post
+        $this->visit('/post/create');
+
+        // user isi `title`, `publish status ` dan content
+        // lalu klik tombol save
+        $this->submitForm('Save', [
+            'title' => 'Belajar Laravel 9 at qadrLabs',
+            'status' => 1, // publish
+            'content' => 'Ini adalah content tutorial belajar laravel 9 di qadrLabs'
+        ]);
+
+        // lihat data post baru di database
+        $this->seeInDatabase('posts', [
+            'title' => 'Belajar Laravel 9 at qadrLabs',
+            'status' => 1,
+            'content' => 'Ini adalah content tutorial belajar laravel 9 di qadrLabs'
+        ]);
+
+        // ter-redirect ke halaman daftar post
+        $this->seePageIs('post');
+
+        // lihat post yang sudah diinput
+        $this->see('Belajar Laravel 9 at qadrLabs');
+        $this->see('Publish');  
     }
 
     /** @test  */
