d00859477b902c3cfb12e5e3cbcfcbffea290828
Gun Gun Priatna <gungunpriatna02@gmail.com>
2022-03-03 06:24:04 +0700

    setup testing

---
+++ tests/Feature/ManagePostsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ManagePostsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function user_can_create_a_post()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test  */
+    public function user_can_browse_posts_index_page()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function user_can_edit_existing_post()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test  */
+    public function user_can_delete_existing_post()
+    {
+        $this->assertTrue(true);
+    }
+}
