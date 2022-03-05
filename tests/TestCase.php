be96cc5bf740accc6e7d5ca376968f78d07bf3f0
Gun Gun Priatna <gungunpriatna02@gmail.com>
2022-03-06 04:38:04 +0700

    fix testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -9,4 +10,18 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
 
     public $baseUrl = 'http://localhost';
+
+    protected function adminUserSigningIn($userDataOverrides = [])
+    {
+        $user = $this->createUser('admin', $userDataOverrides);
+        $this->actingAs($user);
+
+        return $user;
+    }
+    protected function createUser($role='admin', $userDataOverrides = [])
+    {
+        $user = User::factory()->create($userDataOverrides);
+        $user->assignRole($role);
+        return $user;
+    }
 }
